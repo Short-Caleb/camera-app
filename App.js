07a8c8c13092ccf9ff6fc89d0b3127b38473421f
Caleb Short <shortcaleb@gmail.com>
2023-02-01 11:05:47 -0500

    image preview fixed

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, Pressable } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, Pressable, Image } from 'react-native';
 import {Camera, CameraType} from 'expo-camera';
 import React, {useState, useRef, useEffect } from 'react'
 import Feather from '@expo/vector-icons/Feather';
 
+
 export default function App() {
  
- const [cameraType, setCameraType] = useState(CameraType.back);
+ const [cameraType, setCameraType] = useState(CameraType.front);
  const [flashMode, setFlashMode] = useState(Camera.Constants.FlashMode.off);
  const [hasCameraPermission, setHasCameraPermission] = useState(null);
  const [image, setImage] = useState(null);
  
  
  //const cameraRef = useRef(null);
- let cameraRef;
+ let cameraRef = useRef(null);
 
  useEffect(() => {
     (async () => {
@@ -28,24 +29,27 @@ export default function App() {
   );
  };
  
-const takePicture =  () => {
+const takePicture = async () => {
+  console.log("in takePicture")
     if(cameraRef) {
+      console.log("...with camerRef")
       try{
-        const data =  cameraRef.takePicture();
+        const data = await cameraRef.current.takePictureAsync();
         console.log(data);
-        setImage(data.uri);
+        setImage(data);
       } catch (error) {
         console.log(error);
       }
     }
 }
 
+if(!image) {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
      <Camera
      style={styles.camera}
      type={cameraType}
-     ref={ref => (cameraRef = ref)}
+     ref={cameraRef}
      flashMode={flashMode}
      >
     
@@ -55,10 +59,25 @@ const takePicture =  () => {
         <Feather name='circle' style={styles.cameraButton} size={48} color='white' />
         </Pressable>
         </View> 
-    </View>
+    </SafeAreaView>
   );
-}
+} else {
+  return(
+      <SafeAreaView style={styles.container}>
+      <Image source={{uri: image.uri}} style={styles.camera} />
 
+      <View style={styles.controlContainer}>
+        <Pressable onPress={ () => setImage(null)}>
+        <Feather name='circle' style={styles.cameraButton} size={48} color='white' />
+        </Pressable>
+        <Pressable onPress={ () => setImage(null)}>
+        <Feather name='circle' style={styles.cameraButton} size={48} color='white' />
+        </Pressable>
+        </View> 
+    </SafeAreaView> 
+      );
+}
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -75,6 +94,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'black'
+   backgroundColor: 'black'
   },
+
+  previewButton: {
+    
+  }
 });
